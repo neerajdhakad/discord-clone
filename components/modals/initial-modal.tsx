b0ca8c9f5ea8c871e945c0ddcdb4d037d1887edd
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2024-03-10 11:25:27 +0530

    upload-thing-setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import FileUpload from "@/components/file-upload";
 import {
   Dialog,
   DialogContent,
@@ -75,7 +76,21 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO : Image Upload
+                <FormField 
+                control={form.control}
+                name="imageUrl"
+                render={({field})=>(
+                  <FormItem>
+                    <FormControl>
+                      <FileUpload 
+                        endpoint="serverImage"
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+                />
               </div>
               <FormField
                 control={form.control}
