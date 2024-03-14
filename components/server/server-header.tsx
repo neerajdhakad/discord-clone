97037aea11d10f9cc556ee4d89f633d55c51e140
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2024-03-14 16:21:11 +0530

    manage-members-section-completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -57,6 +57,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             {isAdmin && (
                 <DropdownMenuItem
                     className="px-3 py-2 text-sm cursor-pointer"
+                    onClick={()=>onOpen("members",{server})}
                 >
                     Manage Members
                     <Users className="h-4 w-4 ml-auto"/>
