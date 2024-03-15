1e19524b82e7991f10d02f3acffc81686f624a8e
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2024-03-15 18:42:40 +0530

    delete-and-leave-server-routes-completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -83,6 +83,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             {isAdmin && (
                 <DropdownMenuItem
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                    onClick={()=>onOpen("deleteServer",{server})}
                 >
                     Delete Server
                     <Trash className="h-4 w-4 ml-auto"/>
@@ -92,6 +93,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             {!isAdmin && (
                 <DropdownMenuItem
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                    onClick={()=>onOpen("leaveServer",{server})}
                 >
                     Leave Server
                     <LogOut className="h-4 w-4 ml-auto"/>
